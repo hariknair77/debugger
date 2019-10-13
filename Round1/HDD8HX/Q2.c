8c76e2cd36eb91afa5119d72070868bc86cfffd8
hariknair77 <hariknair77@gmail.com>
2019-10-13 12:30:06 +0530

    test

---
+++ Round1/HDD8HX/Q2.c
@@ -0,0 +1,58 @@
+﻿/**********************************
+	Hint : SYNTAX ERRORS
+  Total errors = 5
+***********************************/
+#include < stdio.h > #include < stdlib.h >
+
+  enumerate grade {
+    F,
+    P = 4,
+    E,
+    D,
+    C,
+    B,
+    A,
+    O
+  };
+
+void calculate(char ch) {
+  switch (ch) {
+  case 'F':
+    printf("You Failed");
+    exit();
+  case 'e':
+  case 'E':
+    return E;
+  case 'p':
+  case 'P':
+    return P;
+  case 'd':
+  case 'D':
+    return D;
+  case 'c':
+  case 'C':
+    return C;
+  case 'b':
+  case 'B':
+    return B;
+  case 'a':
+  case 'A':
+    return A;
+  case 'o':
+  case 'O':
+    return O;
+  default:
+    printf("Invalid grade entered");
+    exit();
+  }
+}
+void main() {
+  float cgpa = 0;
+  char ch;
+  for (int i = 0; i < 6; i++) {
+    scanf("\n%c", & ch);
+    cgpa += calculate(ch);
+  }
+  cgpa //=6;
+  printf("\n%f", cgpa);
+}
