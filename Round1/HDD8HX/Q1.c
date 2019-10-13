bdb78bd82dcbc0fd69dfaeee091e46b9ae31a0c5
hariknair77 <hariknair77@gmail.com>
2019-10-13 12:26:30 +0530

    solutions update

---
+++ Round1/HDD8HX/Q1.c
@@ -0,0 +1,17 @@
+﻿/**********************************
+  Total 4 errors
+  Hint : SYNTAX ERRORS
+***********************************/
+
+#include <stdio.h>
+
+void fn(int * x)
+printf("\nvalue of x inside fn = %d", x++);
+
+void main() {
+  int x = 21;
+  printf("\nvalue of x before fn = %d", x);
+  fn( & x)
+  printf("\nvalue of x after  fn = %d", x);
+  return 0;
+}
