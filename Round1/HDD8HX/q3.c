d57cff334e93f7205dd5e1d446e1ae5c3853859a
hariknair77 <hariknair77@gmail.com>
2019-10-13 14:48:42 +0530

    asd

---
+++ Round1/HDD8HX/q3.c
@@ -0,0 +1,18 @@
+﻿/**********************************
+	Total 4 errors
+  Hint : Dangling else
+***********************************/
+#include < stdio.h >
+  void main() {
+    int x = 0, y = 5, z = 4;
+    if (x > y)
+      if (z > x)
+        printf("Largest is %d", z);
+      else if (x > z)
+      printf("Largest is %d", x);
+    else
+    if (y > z)
+      printf("Largest is %d", y);
+    else
+      printf("Largest is %d", z);
+  }
